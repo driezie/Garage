f00ccbd8edee07ac3f2c01535f654d261567c9b3
Drieziee <officialjustdevv@gmail.com>
2022-06-02 11:42:27 +0200

    Create actions.php

---
+++ assets/actions/actions.php
@@ -0,0 +1,15 @@
+<?php
+require_once './functions.php';
+
+
+// if action is set
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'login') {
+        try_login('./login.php');
+    }
+}
+
+
+
+
+?>
